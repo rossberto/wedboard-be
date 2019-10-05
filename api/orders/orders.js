f6a25209d83a873319365fe34d8a6a2ccdd0cda9
ross <dev@rross.me>
2019-10-05 11:31:39 -0500

    GET, PUT and DELETE specific project order added.

--- api/orders/orders.js
+++ api/orders/orders.js
@@ -67,15 +67,15 @@ ordersRouter.post('/', setDataRequirements, mw.validateRequest, mw.getValues, (r
     }
   });
 });
-/*
-ordersRouter.param('projectId', (req, res, next, projectId) => {
-  const sql = `SELECT * FROM Projects WHERE id=${projectId}`;
-  db.query(sql, function(err, project) {
+
+ordersRouter.param('orderId', (req, res, next, orderId) => {
+  const sql = `SELECT * FROM Orders WHERE id=${orderId} AND Projects_id=${req.projectId} LIMIT 1`;
+  db.query(sql, function(err, order) {
     if (err) {
       next(err);
-    } else if (project[0]) {
-      req.projectId = projectId;
-      req.project = project[0];
+    } else if (order[0]) {
+      req.orderId = orderId;
+      req.order = order[0];
       next();
     } else {
       res.status(404).send();
@@ -83,52 +83,50 @@ ordersRouter.param('projectId', (req, res, next, projectId) => {
   });
 });
 
-// GET /api/projects/:projectId
-ordersRouter.get('/:projectId', (req, res, next) => {
-  res.status(200).send({project: req.project});
+
+// GET /api/projects/:projectId/orders/:orderId
+ordersRouter.get('/:orderId', (req, res, next) => {
+  res.status(200).send({order: req.order});
 });
 
-// PUT /api/projects/:projectId
-ordersRouter.put('/:projectId', mw.validateProject, mw.getProjectValues, (req, res, next) => {
-  const sql = 'UPDATE Projects SET ' +
-              'name= ? , ' +
-              'feast_date= ? , ' +
-              'feast_location= ? , ' +
-              'civil_ceremony_date= ? , ' +
-              'civil_ceremony_location= ? , ' +
-              'religious_ceremony_date= ? , ' +
-              'religious_location= ? , ' +
-              'custom_ceremony_description= ? , ' +
-              'custom_ceremony_description_2= ? , ' +
-              'custom_ceremony_date= ? , ' +
-              'custom_ceremony_location= ? , ' +
-              'guests_quantity= ? , ' +
-              'pinterest_board_url= ? ' +
-              `WHERE id=${req.projectId}`;
-
+// PUT /api/projects/:projectId/orders/:orderId
+ordersRouter.put('/:orderId', setDataRequirements, mw.validateRequest, mw.getValues, (req, res, next) => {
+  const sql = 'UPDATE Orders SET ' +
+              'date= ? , ' +
+              'comments= ? , ' +
+              'status= ? , ' +
+              'amount= ? , ' +
+              'Providers_id= ? ' +
+              `WHERE id=${req.orderId} AND Projects_id=${req.projectId}`;
   const values = req.values[0];
-  // We take out non-modifiable values
-  // Quit creationTimestamp
-  values.splice(0,1);
-  // Quit createdBy
-  values.splice(2,1);
 
   db.query(sql, values, function(err) {
     if (err) {
       next(err);
     } else {
-      db.query(`SELECT * FROM Projects WHERE id=${req.projectId}`, function(err, project) {
+      db.query(`SELECT * FROM Orders WHERE id=${req.orderId}`, function(err, order) {
         if (err) {
           next(err);
         } else {
-          res.send({project: project[0]});
+          res.send({order: order[0]});
         }
       });
     }
   });
 });
 
-const projectServicesRouter = require('./projectServices.js');
-ordersRouter.use('/:projectId/services', projectServicesRouter);
+// DELETE /api/projects/:projectId/orders/:orderId
+ordersRouter.delete('/:orderId', (req, res, next) => {
+  db.query(`DELETE FROM Orders WHERE id=${req.orderId} AND Projects_id=${req.projectId}`, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+/*
+const orderServicesRouter = require('./orderServices');
+ordersRouter.use('/:orderId/services', orderServicesRouter);
 */
 module.exports = ordersRouter;
