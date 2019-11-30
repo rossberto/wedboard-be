cd6189f6822805a8cfd6c69dcb97ab8292c3f2df
ross <dev@rross.me>
2019-11-30 15:56:34 -0600

    msql connection change to pool

--- db/database.js
+++ db/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 // Heroku CLEARDB_DATABASE_URL (heroku config | grep CLEARDB_DATABASE_URL)
 // mysql://b7711e16555096:55792152@us-cdbr-iron-east-05.cleardb.net/heroku_0c5078081bd3dc6?reconnect=true
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   multipleStatements: true,
   host    : 'us-cdbr-iron-east-05.cleardb.net',   //'localhost',
   user    : 'b7711e16555096',                     //'root',
