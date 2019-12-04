c2e43d7528dedc7e3d37a2deb0c3536d8224150d
ross <dev@rross.me>
2019-12-04 09:57:46 -0600

    Database connection config for Heroku server

--- db/database.js
+++ db/database.js
@@ -1,23 +1,25 @@
 const mysql = require('mysql');
 
+/* For Cloud server */
 // Heroku CLEARDB_DATABASE_URL (heroku config | grep CLEARDB_DATABASE_URL)
 // mysql://b7711e16555096:55792152@us-cdbr-iron-east-05.cleardb.net/heroku_0c5078081bd3dc6?reconnect=true
-/*
+
 const connection = mysql.createPool({
   multipleStatements: true,
-  host    : 'us-cdbr-iron-east-05.cleardb.net',   //'localhost',
-  user    : 'b7711e16555096',                     //'root',
-  password: '55792152',                           //'contrasena',
-  database: 'heroku_0c5078081bd3dc6'              //'mydb'
+  host    : 'us-cdbr-iron-east-05.cleardb.net',  
+  user    : 'b7711e16555096',
+  password: '55792152',
+  database: 'heroku_0c5078081bd3dc6'
 });
 
-//connection.connect(function(err) {
 connection.getConnection(function(err, connection) {
   if (err) throw err;
 
   console.log("Connected!");
 });
-*/
+
+/* For Local Environment */
+/*
 const connection = mysql.createPool({
   multipleStatements: true,
   host    : 'localhost',
@@ -32,5 +34,5 @@ connection.getConnection(function(err, connection) {
 
   console.log("Connected!");
 });
-
+*/
 module.exports = connection;
